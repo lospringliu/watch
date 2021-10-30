0ebb53115ede0330bf46ada7d7aca2e0e02c8876
Xinchun Liu <xcliu@ca.ibm.com>
2021-10-30 13:51:39 -0400

    pwa

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import YoutubeVideos from './components/YoutubeVideos.vue'
+import { useTimeAgo } from '@vueuse/core'
+import ReloadPrompt from './components/ReloadPrompt.vue'
+// replaced dyanmicaly
+const date = '__DATE__'
+const timeAgo = useTimeAgo(date)
 </script>
 
 <template>
   <YoutubeVideos />
+  <div class="text-blue-500">Built {{ timeAgo }}</div>
+  <ReloadPrompt />
 </template>
