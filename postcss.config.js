1d52a26354ea0a1e9d557312dfce282dab7df3fb
Xinchun Liu <xcliu@ca.ibm.com>
2022-02-14 20:15:54 -0500

    retry windi

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   plugins: {
+    // require('postcss-import'),
+    // require('tailwindcss/nesting')(require('postcss-nesting')),
+    // require('tailwindcss'),
+    // require('autoprefixer'),
     tailwindcss: {},
     autoprefixer: {},
   },
