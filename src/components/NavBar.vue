8c6efe59cb0636a474aa9fe19cdfba4c70ae1e89
Xinchun Liu <xcliu@ca.ibm.com>
2022-02-24 20:31:21 -0500

    nav

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -29,13 +29,13 @@ const color = useColor('light')
 
 <template lang="pug">
 .flex.flex-col
-  a.fixed.left-0.top-0.z-1000.hidden.sm_block(href="/#")
+  a.fixed.left-0.top-0.z-1000(href="/#")
     p.font-bold.text-6xl.text-left.text-green-500.text-opacity-50.write-vertical-right.text-stroke-sm.text-stroke-blue-500 W
   .min-h-4vh.md_min-h-6vh.flex.flex-wrap.items-center.gap-2.p-2.bg-light-900.shadow-xl.z-400.sticky.w-full.bg-cover.top-0(
     :style="{ ...bg }"
     )
-    .w-4.h-12
-    .flex-1.hidden.sm_block
+    .w-8.h-12
+    .flex-1
     router-link.link(
       v-for="(link, l) in routes" :key="link" 
       :to="l" ) 
@@ -46,7 +46,7 @@ const color = useColor('light')
       ph-chats-teardrop(v-if="link == 'Chats'")
       ph-users(v-if="link == 'Users'")
       .hidden.md_block {{ link }}
-    .flex-1.hidden.sm_block
+    .flex-1
     user-icon(
       :size="40"
       @user="$router.push(`/users/${$event}`)" @room="$router.push(`/rooms/${$event}`)"
