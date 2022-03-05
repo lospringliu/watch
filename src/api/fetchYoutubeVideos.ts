4e160d4b906b7676184fa038be91e9b0ec8ee785
Xinchun Liu <xcliu@ca.ibm.com>
2022-03-05 18:13:13 -0500

    load videos

--- src/api/fetchYoutubeVideos.ts
+++ src/api/fetchYoutubeVideos.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { IVideo, IChannel } from "../types"
 import AsyncForEach from "async-await-foreach"
 import { videos, prefers } from "../stores"
-import { put_video, initChannels } from "~/composables/useVideos"
+import { put_video, initChannels, initVideos } from "~/composables/useVideos"
 const gapi = axios.create({ baseURL: 'https://youtube.googleapis.com/youtube/v3/' })
 gapi.defaults.headers.common.Accept = 'application/json'
 
@@ -11,7 +11,7 @@ function makeParams(options = {}) {
     params: Object.assign({}, {
       part: 'snippet,contentDetails',
       // maxResults: Math.min(50, prefers.maxResults),
-      maxResults: Math.min(5, prefers.maxResults),
+      maxResults: Math.min(50, prefers.maxResults),
     }, options),
   }
 }
@@ -20,9 +20,10 @@ export async function fetchYoutubeVideos (channels: IChannel[] = []) {
   await initChannels()
   if (!prefers.youtubeAppKey) {
     console.log(`... bypass fetching videos no api key`)
+    await initVideos()
     return videos.videos
   }
-  await AsyncForEach(channels.slice(0,2), async (channel) => {
+  await AsyncForEach(channels, async (channel) => {
     const is_channel = channel.id.startsWith('UC')
     let playlistId = channel.id
     try {
