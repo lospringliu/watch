33aa1272d64b5a6ffd13f86d9fc8d816be5f8a82
Xinchun Liu <xcliu@ca.ibm.com>
2022-03-04 21:20:51 -0500

    direct3

--- src/App.vue
+++ src/App.vue
@@ -16,18 +16,16 @@ const timeAgo = useTimeAgo(date)
 </script>
 
 <template>
-  <div>
-    <div class="bg-black min-w-xs md_px-12 lg_px-24 xl_px-48">
-      <YoutubePlayer />
-    </div>
-    <div class="bg-cyan-300">
-      <Navier />
-    </div>
-    <div class="bg-white dark_bg-gray-800 grid grid-col grid-cols-1 gap-2 min-w-xs place-content-center sm_px-2 sm_gap-4 md_px-4 md_gap-6 md_grid-cols-2 lg_gap-8 lg_grid-cols-3 xl_grid-cols-4 text-center text-gray-700 dark_text-gray-200">
-      <YoutubeVideos />
-    </div>
-    <div class="text-center text-blue-500">Built {{ timeAgo }}</div>
+  <div class="bg-black min-w-xs md_px-12 lg_px-24 xl_px-48">
+    <YoutubePlayer />
   </div>
+  <div class="bg-cyan-300">
+    <Navier />
+  </div>
+  <div class="bg-white dark_bg-gray-800 grid grid-col grid-cols-1 gap-2 min-w-xs place-content-center sm_px-2 sm_gap-4 md_px-4 md_gap-6 md_grid-cols-2 lg_gap-8 lg_grid-cols-3 xl_grid-cols-4 text-center text-gray-700 dark_text-gray-200">
+    <YoutubeVideos />
+  </div>
+  <div class="text-center text-blue-500">Built {{ timeAgo }}</div>
 </template>
 
 <style lang="postcss">
@@ -41,6 +39,6 @@ body {
   overscroll-behavior-y: contain;
 }
 #app {
-  @apply min-h-100vh flex flex-col;
+  @apply w-100vw min-h-100vh flex flex-col;
 }
 </style>
