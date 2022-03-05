9ebdfbd4ae5b91167f3b08b34fdb74ce6e744312
Xinchun Liu <xcliu@ca.ibm.com>
2022-03-04 21:07:34 -0500

    direct2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,5 @@
 <script setup lang="ts">
-import { watch, watchEffect, computed } from "vue";
-import { currentRoom, rootRoom, useBackground } from "@composables";
-import Videos from "@/pages/videos.vue"
+import { currentRoom, useBackground } from "@composables";
 
 
 //  nav-bar
@@ -12,10 +10,24 @@ const bg = computed(() => useBackground({ pub: currentRoom.pub, size: 1200, ligh
 //   component(:is="Videos")
 // .p-0.flex.flex-col(style="flex: 1000 1 100%" :style="{ ...bg }")
 // transition(name="fade" mode="out-in")
+import { useTimeAgo } from '@vueuse/core'
+const date = '__DATE__'
+const timeAgo = useTimeAgo(date)
 </script>
 
-<template lang="pug">
-Videos
+<template>
+  <div>
+    <div class="bg-black min-w-xs md_px-12 lg_px-24 xl_px-48">
+      <YoutubePlayer />
+    </div>
+    <div class="bg-cyan-300">
+      <Navier />
+    </div>
+    <div class="bg-white dark_bg-gray-800 grid grid-col grid-cols-1 gap-2 min-w-xs place-content-center sm_px-2 sm_gap-4 md_px-4 md_gap-6 md_grid-cols-2 lg_gap-8 lg_grid-cols-3 xl_grid-cols-4 text-center text-gray-700 dark_text-gray-200">
+      <YoutubeVideos />
+    </div>
+    <div class="text-center text-blue-500">Built {{ timeAgo }}</div>
+  </div>
 </template>
 
 <style lang="postcss">
