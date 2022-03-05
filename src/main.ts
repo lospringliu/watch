ac3aa413e728b6b298d610bec6c11eb2b67168b1
Xinchun Liu <xcliu@ca.ibm.com>
2022-03-05 11:50:21 -0500

    init gun for mobile

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,24 @@
 import { createApp } from "vue";
-import App from "./App.vue";
-import { useVideos } from "./composables/useVideos"
 import platform from 'platform-detect'
 import { globalState } from './stores/globalState'
 
 import "virtual:windi.css";
 import "@components/styles/index.css";
-
 globalState.platform = platform
+
+import { useGun, useGun2 } from "@composables/gun"
+let gun, gun2
+if (globalState.platform.android) {
+  gun = useGun({})
+  gun2 = useGun2({})
+} else {
+  gun = useGun()
+  gun = useGun2()
+}
+
+import App from "./App.vue";
+import { useVideos } from "./composables/useVideos"
+
 watch(globalState, (value, old_value) => {
   console.log(`... watched ...`)
   console.log(value)
