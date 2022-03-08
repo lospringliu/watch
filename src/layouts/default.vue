e15499128ff1aa87e8579d35fe3082931db06248
Xinchun Liu <xcliu@ca.ibm.com>
2022-03-08 12:34:46 -0500

    bottom nav

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <main>
-    <nav-bar />
-    <ReloadPrompt />
     <router-view />
+    <ReloadPrompt />
+    <div class="fixed left-0 bottom-0 z-400 inset-x-0 opacity-99">
+      <nav-bar />
+    </div>
   </main>
 </template>
