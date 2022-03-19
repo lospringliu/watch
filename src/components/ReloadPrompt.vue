cbf83b17958a2cfefc1dbfc636cf55e49aefa8e4
Xinchun Liu <xcliu@ca.ibm.com>
2022-03-19 11:45:43 -0400

    reload

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -25,6 +25,30 @@ const close = async() => {
   offlineReady.value = false
   needRefresh.value = false
 }
+//  <div
+//    v-if="offlineReady || needRefresh"
+//    class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-teal-500"
+//    role="alert"
+//  >
+//    <div class="mb-1">
+//      <span v-if="offlineReady">
+//        {{ $t('pages.prompt_offline') }}
+//      </span>
+//      <span v-else>
+//        {{ $t('pages.prompt_reload') }}
+//      </span>
+//    </div>
+//    <div class="flex justify-around rounded-lg">
+//      <button v-if="needRefresh" @click="updateServiceWorker()" class="button text-cyan-600">
+//        Reload
+//        {{ $t('button.reload') }}
+//      </button>
+//      <button @click="close" class="button text-cyan-400">
+//        Close
+//        {{ $t('button.close') }}
+//      </button>
+//    </div>
+//  </div>
 </script>
 
 <template>
@@ -50,33 +74,6 @@ const close = async() => {
   </div>
 </template>
 
-
-<template>
-  <div
-    v-if="offlineReady || needRefresh"
-    class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-teal-500"
-    role="alert"
-  >
-    <div class="mb-1">
-      <span v-if="offlineReady">
-        {{ $t('pages.prompt_offline') }}
-      </span>
-      <span v-else>
-        {{ $t('pages.prompt_reload') }}
-      </span>
-    </div>
-    <div class="flex justify-around rounded-lg">
-      <button v-if="needRefresh" @click="updateServiceWorker()" class="button text-cyan-600">
-        Reload
-        {{ $t('button.reload') }}
-      </button>
-      <button @click="close" class="button text-cyan-400">
-        Close
-        {{ $t('button.close') }}
-      </button>
-    </div>
-  </div>
-</template>
 
 <style>
 .pwa-toast {
