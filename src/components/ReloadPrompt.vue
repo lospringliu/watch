7d7d3632211284e310bb649eae2f9ef028d97284
Xinchun Liu <xcliu@ca.ibm.com>
2022-03-19 13:05:13 -0400

    reload

--- src/components/ReloadPrompt.vue
+++ src/components/ReloadPrompt.vue
@@ -27,53 +27,52 @@ const close = async() => {
 }
 //  <div
 //    v-if="offlineReady || needRefresh"
-//    class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-teal-500"
+//    class="pwa-toast"
 //    role="alert"
 //  >
-//    <div class="mb-1">
+//    <div class="message">
 //      <span v-if="offlineReady">
-//        {{ $t('pages.prompt_offline') }}
+//        App ready to work offline
 //      </span>
 //      <span v-else>
-//        {{ $t('pages.prompt_reload') }}
+//        New content available, click on reload button to update.
 //      </span>
 //    </div>
-//    <div class="flex justify-around rounded-lg">
-//      <button v-if="needRefresh" @click="updateServiceWorker()" class="button text-cyan-600">
-//        Reload
-//        {{ $t('button.reload') }}
-//      </button>
-//      <button @click="close" class="button text-cyan-400">
-//        Close
-//        {{ $t('button.close') }}
-//      </button>
-//    </div>
+//    <button v-if="needRefresh" @click="updateServiceWorker()">
+//      Reload
+//    </button>
+//    <button @click="close">
+//      Close
+//    </button>
 //  </div>
 </script>
 
 <template>
   <div
     v-if="offlineReady || needRefresh"
-    class="pwa-toast"
+    class="text-white px-6 py-4 border-0 rounded relative mb-4 bg-teal-500"
     role="alert"
   >
-    <div class="message">
+    <div class="mb-1">
       <span v-if="offlineReady">
-        App ready to work offline
+        {{ $t('pages.prompt_offline') }}
       </span>
       <span v-else>
-        New content available, click on reload button to update.
+        {{ $t('pages.prompt_reload') }}
       </span>
     </div>
-    <button v-if="needRefresh" @click="updateServiceWorker()">
-      Reload
-    </button>
-    <button @click="close">
-      Close
-    </button>
+    <div class="flex justify-around rounded-lg">
+      <button v-if="needRefresh" @click="updateServiceWorker()" class="button text-cyan-600">
+        Reload
+        {{ $t('button.reload') }}
+      </button>
+      <button @click="close" class="button text-cyan-400">
+        Close
+        {{ $t('button.close') }}
+      </button>
+    </div>
   </div>
 </template>
-
 
 <style>
 .pwa-toast {
