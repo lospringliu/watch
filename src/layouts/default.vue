287315642f46c0d8a68a3ed5bfb25b878c063602
Xinchun Liu <xcliu@ca.ibm.com>
2022-04-03 23:23:34 -0400

    layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,12 +1,11 @@
 <template>
-  <main>
-    <NavSocial v-if="globalState.show_social" />
-    <Navier v-if="globalState.show_tools" />
-    <ReloadPrompt />
-    <router-view />
-    <div :style="style">
-      <hr>
+  <main class="h-100vh w-100vw" :style="style">
+    <div ref="navbar" class="fixed left-0 top-0 z-400 inset-x-0 opacity-60">
+      <NavSocial v-if="globalState.show_social" />
+      <Navier v-if="globalState.show_tools" />
+      <ReloadPrompt />
     </div>
+    <router-view />
     <div ref="navbar" class="fixed left-0 bottom-0 z-400 inset-x-0 opacity-99">
       <NavBar />
     </div>
@@ -25,5 +24,6 @@ const navbar = ref(null)
 const { height } = useElementSize(navbar)
 // const clstop = computed(() => `top-${top.value}`)
 // const stltop = computed(() => `padding-top: ${top.value === "0px" ? +top.value.replace("px","") - 21 : 0}px;`)
-const style = computed(() => `padding-top: ${Math.floor(height.value)}px;`)
+// const style = computed(() => `padding-top: ${Math.floor(height.value)}px;`)
+const style = computed(() => `padding-bottom: ${Math.floor(height.value)}px;`)
 </script>
